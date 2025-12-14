bc7d41f48b72999e6a58a3b10f742d2ad0e414ad
Afonso Ferreira <afonso.ferreira@ua.pt>
2025-12-14 17:42:13 +0000

    fixing

--- paper-magic/level_select.gd
+++ paper-magic/level_select.gd
@@ -1,25 +1,23 @@
 extends Control
 
 @export var level_btn_scene : PackedScene 
-# This line creates a list in the Inspector
-@export var all_levels : Array[PackedScene] 
+
+# --- FIX: Change Array[PackedScene] to Array[String] ---
+# This stores the path "res://Level1.tscn" instead of the scene itself.
+# The '*.tscn' part filters the file picker to only show scene files.
+@export_file("*.tscn") var all_levels : Array[String] 
+# -------------------------------------------------------
 
 @onready var grid = $MarginContainer/VBoxContainer/ScrollContainer/GridContainer
 
 func _ready():
-	# Loop through your Array of levels
 	for i in range(all_levels.size()):
 		var btn = level_btn_scene.instantiate()
 		
-		# --- CHANGE THIS LINE ---
-		# Old: btn.get_node("Label").text = str(i + 1)
-		
-		# New: Add "Level " + the number
 		btn.get_node("Label").text = "Level " + str(i + 1)
-		# ------------------------
 		
-		# Assign the scene path from the array
-		if all_levels[i]:
-			btn.level_path = all_levels[i].resource_path
+		# Now we assign the string directly, which is safe!
+		if all_levels[i] != "":
+			btn.level_path = all_levels[i]
 			
 		grid.add_child(btn)
